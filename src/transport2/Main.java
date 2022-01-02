2d2192d585eeef68e029a8fc5cebb019a789e03d
unknown <chico22999@gmail.com>
2022-01-02 19:25:33 +0200

    More features

--- src/transport2/Main.java
+++ src/transport2/Main.java
@@ -7,86 +7,84 @@ public class Main {
 
 	public static void main(String args[]) {
 		    
-		Driver dr = new Driver();
-		dr.DriverArrivedToLocation("Tom", 21);
-//		    DataBase db= new DataBase();
-//			User theuser=new User();
-//			Admin a=new Admin();
-//			
-//			User user1=new User();
-//			user1.setUsername("Aliaa Ahmed");
-//			user1.setIdentity("customer");
-//			user1.setMobilenumber("01005515727");
-//			user1.setPassword("111111");
-//			
-//			User user2=new User();
-//			user2.setUsername("Amira Ahmed");
-//			user2.setIdentity("customer");
-//			user2.setMobilenumber("0107415698");
-//			user2.setPassword("222222");
-//			
-//			/*a.users.add(user1);
-//			a.users.add(user2);
-//			a.systemcustomers.add(user1);
-//			a.systemcustomers.add(user2);*/
-//			db.users.add(user1);
-//			db.users.add(user2);
-//			db.systemcustomers.add(user1);
-//			db.systemcustomers.add(user2);
-//			
-//			Driver driver1=new Driver();
-//			driver1.setUsername("Sherief Mohammed");
-//			driver1.setIdentity("driver");
-//			driver1.setMobilenumber("0108569345");
-//			driver1.setPassword("333333");
-//			driver1.setLicensenum("64654655613646465");
-//			driver1.setNationalid("986545454564646");
-//			driver1.FavoriteAreas.put("Giza"   , 30);
-//			driver1.FavoriteAreas.put("Dokki"  , 40);
-//			driver1.FavoriteAreas.put("October", 60);
-//			
-//			//a.users.add(driver1);
-//			//a.systemdrivers.add(driver1);
-//			db.users.add(driver1);
-//			db.systemdrivers.add(driver1);
-//			
-//			Driver driver2=new Driver();
-//			driver2.setUsername("Ahmed Akram");
-//			driver2.setIdentity("driver");
-//			driver2.setMobilenumber("0108578746854");
-//			driver2.setPassword("222555");
-//			driver2.setLicensenum("64545456465jnjcc");
-//			driver2.setNationalid("9865459867464646");
-//			driver2.FavoriteAreas.put("Giza",10);
-//
-//			//a.users.add(driver2);
-//			//a.systemdrivers.add(driver2);
-//			db.users.add(driver2);
-//			db.systemdrivers.add(driver2);
-//			
-//			Admin adm=new Admin();
-//			adm.setUsername("ghada mohammed");
-//			adm.setIdentity("admin");
-//			adm.setMobilenumber("741258963258");
-//			adm.setPassword("555555");
-//			//a.Admins.add(adm);
-//			//a.users.add(adm);
-//			db.Admins.add(adm);
-//			db.users.add(adm);
-//			
-//			
-//			System.out.println("              Welcome to Drive pplication        ");
-//			System.out.println("Enter 1: Login");
-//			System.out.println("Enter 2: Register new account");
-//			int choice1=sc.nextInt();
-//			if(choice1==1)
-//			{
-//				theuser.Login(db);
-//			}
-//			else if(choice1==2)
-//			{
-//				theuser.Registeration(db);
-//				//a.Registeration();
-//			}
+		    DataBase db= new DataBase();
+			User theuser=new User();
+			Admin a=new Admin();
+			
+			User user1=new User();
+			user1.setUsername("Aliaa Ahmed");
+			user1.setIdentity("customer");
+			user1.setMobilenumber("01005515727");
+			user1.setPassword("111111");
+			
+			User user2=new User();
+			user2.setUsername("Amira Ahmed");
+			user2.setIdentity("customer");
+			user2.setMobilenumber("0107415698");
+			user2.setPassword("222222");
+			
+			/*a.users.add(user1);
+			a.users.add(user2);
+			a.systemcustomers.add(user1);
+			a.systemcustomers.add(user2);*/
+			db.users.add(user1);
+			db.users.add(user2);
+			db.systemcustomers.add(user1);
+			db.systemcustomers.add(user2);
+			
+			Driver driver1=new Driver();
+			driver1.setUsername("Sherief Mohammed");
+			driver1.setIdentity("driver");
+			driver1.setMobilenumber("0108569345");
+			driver1.setPassword("333333");
+			driver1.setLicensenum("64654655613646465");
+			driver1.setNationalid("986545454564646");
+			driver1.FavoriteAreas.put("Giza"   , 30);
+			driver1.FavoriteAreas.put("Dokki"  , 40);
+			driver1.FavoriteAreas.put("October", 60);
+			
+			//a.users.add(driver1);
+			//a.systemdrivers.add(driver1);
+			db.users.add(driver1);
+			db.systemdrivers.add(driver1);
+			
+			Driver driver2=new Driver();
+			driver2.setUsername("Ahmed Akram");
+			driver2.setIdentity("driver");
+			driver2.setMobilenumber("0108578746854");
+			driver2.setPassword("222555");
+			driver2.setLicensenum("64545456465jnjcc");
+			driver2.setNationalid("9865459867464646");
+			driver2.FavoriteAreas.put("Giza",10);
+
+			//a.users.add(driver2);
+			//a.systemdrivers.add(driver2);
+			db.users.add(driver2);
+			db.systemdrivers.add(driver2);
+			
+			Admin adm=new Admin();
+			adm.setUsername("ghada mohammed");
+			adm.setIdentity("admin");
+			adm.setMobilenumber("741258963258");
+			adm.setPassword("555555");
+			//a.Admins.add(adm);
+			//a.users.add(adm);
+			db.Admins.add(adm);
+			db.users.add(adm);
+			
+			
+			System.out.println("              Welcome to Drive pplication        ");
+			System.out.println("Enter 1: Login");
+			System.out.println("Enter 2: Register new account");
+			int choice1=sc.nextInt();
+			if(choice1==1)
+			{
+				theuser.Login(db);
+			}
+			else if(choice1==2)
+			{
+				theuser.Registeration(db);
+				//a.Registeration();
+			}
 	}
 }
